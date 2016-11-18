07924f85dccc37cf3f947cebd51bb5f8dd5b2782
RailsonTales <railson777@gmail.com>
2016-11-18 17:22:54 -0200

    PolitiKart
    Jogo

---
+++ Neon Racers 1.0/Assets/Scripts/Menu.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 150, 100, 50), "Jogar"))
+        {
+            Application.LoadLevel("principal");
+        }
+        if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 100, 50), "Ajuda"))
+        {
+            Application.LoadLevel("ajuda");
+        }
+        if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 50, 100, 50), "Créditos"))
+        {
+            Application.LoadLevel("creditos");
+        }
+        if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 150, 100, 50), "Sair"))
+        {
+            Application.Quit();
+        }
+    }
+
+}
---
+++ Neon Racers 1.0/Assets/Scripts/Voltar.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Voltar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 150, 100, 50), "Voltar"))
+        {
+            Application.LoadLevel("menu");
+        }
+    }
+}
